3b2b547f553ffa631c713e80c5383054e8c6ec87
Jean-Simon Cormier <jean.simon.cormier@outlook.com>
2018-11-04 12:37:41 -0500

    Added conversion

--- brx_converter_cli/brx_converter_cli/Controllers/MainController.cs
+++ brx_converter_cli/brx_converter_cli/Controllers/MainController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace brx_converter_cli.Controllers
@@ -40,6 +41,44 @@ namespace brx_converter_cli.Controllers
                     FilesToConvert.Add(file);
                 }
             }
+        }
+
+        public void ConvertAll()
+        {
+            foreach(string file in FilesToConvert)
+            {
+                ClearLine();
+                Console.Write("Converting " + file);
+                byte[] bytes = System.IO.File.ReadAllBytes(file);
+                byte[] reversed = InvertBytesArray(bytes);
+                string outputPath = _outputFolder + file.Replace(_inputFolder, "");
+                outputPath = outputPath.Substring(0, outputPath.Length - 4) + ".mp3";
+                string outputPathFolder = Path.GetDirectoryName(outputPath);
+                if(!Directory.Exists(outputPathFolder))
+                {
+                    Directory.CreateDirectory(outputPathFolder);
+                }
+                System.IO.File.WriteAllBytes(outputPath, reversed);
+            }
+        }
+
+        private byte[] InvertBytesArray(byte[] bytes)
+        {
+            List<byte> reversed = new List<byte>();
+            foreach(byte @byte in bytes)
+            {
+                byte reversedByte = @byte;
+                reversedByte ^= byte.MaxValue;
+                reversed.Add(reversedByte);
+            }
+            return reversed.ToArray();
+        }
+
+        private void ClearLine()
+        {
+            Console.SetCursorPosition(0, Console.CursorTop);
+            Console.Write(new string(' ', Console.WindowWidth));
+            Console.SetCursorPosition(0, Console.CursorTop - 1);
         }
     }
 }
--- brx_converter_cli/brx_converter_cli/Program.cs
+++ brx_converter_cli/brx_converter_cli/Program.cs
@@ -10,11 +10,8 @@ namespace brx_converter_cli
             var inputFolder = args[0];
             var outputFolder = args[1];
             var mainController = new MainController(inputFolder, outputFolder);
-            foreach(string file in mainController.FilesToConvert)
-            {
-                Console.WriteLine(file);
-            }
             Console.WriteLine("Found " + mainController.FilesToConvert.Count + " files to convert in " + inputFolder);
+            mainController.ConvertAll();
             Console.ReadLine();
         }
     }
