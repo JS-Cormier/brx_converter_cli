c6987aa6781e8a681c0e344342bd022e3c14e352
Jean-Simon Cormier <jean.simon.cormier@outlook.com>
2018-11-03 23:41:35 -0400

    Added project to repo

---
+++ brx_converter_cli/brx_converter_cli/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace brx_converter_cli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputFolder = args[0];
+            var outputFolder = args[1];
+            Console.WriteLine("Input:" + inputFolder + "/ Output:" + outputFolder);
+            Console.ReadLine();
+        }
+    }
+}
